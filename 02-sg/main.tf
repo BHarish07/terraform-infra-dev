6d5c813617170cc45e8cd7f3ff89c2274a4eb844
BHarish07 <harishbalike1995@gmail.com>
2024-06-18 11:11:11 +0530

    backend-deploy

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -133,6 +133,14 @@ resource "aws_security_group_rule" "backend_bastion" {
   security_group_id = module.backend.sg_id
 }
 
+resource "aws_security_group_rule" "backend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["172.31.0.0/16"]
+  security_group_id = module.backend.sg_id
+}
 
 resource "aws_security_group_rule" "app_alb_vpn" {
   type              = "ingress"
